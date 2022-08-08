5a69b92ffb79fa1ff56bd59e5f4b90999b37c4b7
Feng-Zhang <fengzhang0709@hotmail.com>
2022-08-08 10:37:32 +0800

    add code for chapter6

--- workflow-scripts/chapter6.R
+++ workflow-scripts/chapter6.R
@@ -1,4 +1,24 @@
-rm(list=ls());options(stringsAsFactors = F)
+# set path ------------------------------------------
+rm(list=ls());options(stringsAsFactors=FALSE)
+project_dir <- rprojroot::find_rstudio_root_file()
+raw_dir <- file.path(project_dir,"raw-data")
+process_dir <- file.path(project_dir,"process-data")
+output_dir <- file.path(project_dir,"output-results")
+temp_dir <- file.path(project_dir,"temp-data")
+bin_dir <- file.path(project_dir,"bin")
+script_dir <- file.path(project_dir,"workflow-scripts")
+work_dir <- output_dir
+setwd(work_dir)
+
+# library   -----------------------------------------
+library(tidyverse)
+
+# set default parameter  ---------------------------
+prefix <- "6-"
+output_prefix <- file.path(work_dir,prefix)
+process_prefix <-  file.path(process_dir,prefix)
+
+# perpare data ------------------------------
 #--- loading ---#
 library(MouseGastrulationData)
 sce.chimera <- WTChimeraData(samples=5:10)
@@ -43,3 +63,28 @@ colLabels(merged) <- factor(clusters$membership)
 #--- dimensionality-reduction ---#
 merged <- runTSNE(merged, dimred="corrected", external_neighbors=TRUE)
 merged <- runUMAP(merged, dimred="corrected", external_neighbors=TRUE)
+save(merged,file=paste0(output_prefix,"merged.RData"))
+
+# analysis --------------------
+abundances <- table(merged$celltype.mapped, merged$sample) 
+abundances <- unclass(abundances) 
+head(abundances)
+
+library(edgeR)
+# Attaching some column metadata.
+extra.info <- colData(merged)[match(colnames(abundances), merged$sample),]
+y.ab <- DGEList(abundances, samples=extra.info)
+y.ab
+
+keep <- filterByExpr(y.ab, group=y.ab$samples$tomato)
+y.ab <- y.ab[keep,]
+summary(keep)
+
+design <- model.matrix(~factor(pool) + factor(tomato), y.ab$samples)
+y.ab <- estimateDisp(y.ab, design, trend="none")
+summary(y.ab$common.dispersion)
+fit.ab <- glmQLFit(y.ab, design, robust=TRUE, abundance.trend=FALSE)
+summary(fit.ab$var.prior)
+res <- glmQLFTest(fit.ab, coef=ncol(design))
+summary(decideTests(res))
+topTags(res)
